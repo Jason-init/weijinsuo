6a8f3535e6d6d7f494710673baa735d07f90ac38
QiZhang <jason.qizhang.b@gmail.com>
2020-05-04 22:17:55 +0800

    Implemented carousel images for different devices

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
 /*
-* @Author: QiZhang
-* @Date:   2020-05-01 20:21:11
-* @Last Modified by:   QiZhang
-* @Last Modified time: 2020-05-01 20:21:11
-*/
+ * @Author: QiZhang
+ * @Date:   2020-05-01 20:21:11
+ * @Last Modified by:   QiZhang
+ * @Last Modified time: 2020-05-04 22:14:07
+ */
+$(function() {
+	function resize() {
+ 		var windowWidth = $(window).width()
+ 		$('#main_ad > .carousel-inner > .item').each(function(idex, item) {
+ 			var imgSrc = windowWidth < 768 ? $(item).data('image-xs') : $(item).data('image-lg')
+ 			$(item).css('backgroundImage', 'url("' + imgSrc + '")')
+ 		})
+ 	}
+ 	$(window).on('resize', resize).trigger('resize')
+})
