f980e532c331b7a25a705847c4f8f6828e55fd3b
QiZhang <jason.qizhang.b@gmail.com>
2020-05-05 09:19:50 +0800

    Fixed carouse bugs

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
  * @Author: QiZhang
  * @Date:   2020-05-01 20:21:11
  * @Last Modified by:   QiZhang
- * @Last Modified time: 2020-05-04 22:14:07
+ * @Last Modified time: 2020-05-05 09:17:33
  */
 $(function() {
 	function resize() {
@@ -10,6 +10,13 @@ $(function() {
  		$('#main_ad > .carousel-inner > .item').each(function(idex, item) {
  			var imgSrc = windowWidth < 768 ? $(item).data('image-xs') : $(item).data('image-lg')
  			$(item).css('backgroundImage', 'url("' + imgSrc + '")')
+
+ 			if(windowWidth < 768){
+ 				$(item).html('<img src="' + imgSrc + '" alt="" />')
+ 			}
+ 			else{
+ 				$(item).empty()
+ 			}
  		})
  	}
  	$(window).on('resize', resize).trigger('resize')
