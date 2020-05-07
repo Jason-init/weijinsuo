4e178ff4cbfb5d7b8f6b8cc390202982f2cb45d8
QiZhang <jason.qizhang.b@gmail.com>
2020-05-07 20:31:47 +0800

    Implemented ul on tabs to make it scrollable when screen is small

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
  * @Author: QiZhang
  * @Date:   2020-05-01 20:21:11
  * @Last Modified by:   QiZhang
- * @Last Modified time: 2020-05-07 17:44:09
+ * @Last Modified time: 2020-05-07 20:23:15
  */
 $(function() {
 	function resize() {
@@ -19,6 +19,15 @@ $(function() {
  			}
  		})
  	}
+
  	$(window).on('resize', resize).trigger('resize')
+
  	$('[data-toggle="tooltip"]').tooltip()
+
+ 	var width = 30
+ 	$('.ul-wrapper > .nav-tabs').children().each(function (i, item) {
+ 		width += $(item).width()
+ 	})
+ 	$('.ul-wrapper > .nav-tabs').css('width', width)
+
 })
