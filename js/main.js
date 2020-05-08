dd120a9eb86e8f6b71e8b41dc898b4f4e17bad08
QiZhang <jason.qizhang.b@gmail.com>
2020-05-08 17:00:13 +0800

    Fixed bugs on ul-wrapper

--- js/main.js
+++ js/main.js
@@ -2,32 +2,28 @@
  * @Author: QiZhang
  * @Date:   2020-05-01 20:21:11
  * @Last Modified by:   QiZhang
- * @Last Modified time: 2020-05-07 20:23:15
+ * @Last Modified time: 2020-05-08 16:57:37
  */
 $(function() {
-	function resize() {
- 		var windowWidth = $(window).width()
- 		$('#main_ad > .carousel-inner > .item').each(function(idex, item) {
- 			var imgSrc = windowWidth < 768 ? $(item).data('image-xs') : $(item).data('image-lg')
- 			$(item).css('backgroundImage', 'url("' + imgSrc + '")')
-
- 			if(windowWidth < 768){
- 				$(item).html('<img src="' + imgSrc + '" alt="" />')
- 			}
- 			else{
- 				$(item).empty()
- 			}
- 		})
- 	}
-
- 	$(window).on('resize', resize).trigger('resize')
-
- 	$('[data-toggle="tooltip"]').tooltip()
-
- 	var width = 30
- 	$('.ul-wrapper > .nav-tabs').children().each(function (i, item) {
- 		width += $(item).width()
- 	})
- 	$('.ul-wrapper > .nav-tabs').css('width', width)
-
+    function resize() {
+        var windowWidth = $(window).width()
+        $('#main_ad > .carousel-inner > .item').each(function(idex, item) {
+            var imgSrc = windowWidth < 768 ? $(item).data('image-xs') : $(item).data('image-lg')
+            $(item).css('backgroundImage', 'url("' + imgSrc + '")')
+            if (windowWidth < 768) {
+                $(item).html('<img src="' + imgSrc + '" alt="" />')
+            } else {
+                $(item).empty()
+            }
+        })
+    }
+    $(window).on('resize', resize).trigger('resize')
+    $('[data-toggle="tooltip"]').tooltip()
+    var width = 30
+    $('.ul-wrapper > .nav-tabs').children().each(function(i, item) {
+        width += $(item).width()
+    })
+    if ($(window).width() < width) {
+        $('.ul-wrapper > .nav-tabs').css('width', width).parent().css('overflow-x', 'scroll')
+    }
 })
