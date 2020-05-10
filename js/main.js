892e8b1c4820d03f314b97db6c5c8a31e37c141d
QiZhang <jason.qizhang.b@gmail.com>
2020-05-10 18:27:33 +0800

    Added touchmove function for carousel when web is running on mobile device

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
  * @Author: QiZhang
  * @Date:   2020-05-01 20:21:11
  * @Last Modified by:   QiZhang
- * @Last Modified time: 2020-05-09 20:38:23
+ * @Last Modified time: 2020-05-10 18:22:09
  */
 $(function() {
     function resize() {
@@ -18,9 +18,9 @@ $(function() {
         })
     }
     $(window).on('resize', resize).trigger('resize')
-
+    //
     $('[data-toggle="tooltip"]').tooltip()
-
+    //
     var width = 30
     $('.ul-wrapper > .nav-tabs').children().each(function(i, item) {
         width += $(item).width()
@@ -28,9 +28,23 @@ $(function() {
     if ($(window).width() < width) {
         $('.ul-wrapper > .nav-tabs').css('width', width).parent().css('overflow-x', 'scroll')
     }
-
-    $('#news .nav-pills a').on('click', function () {
+    $('#news .nav-pills a').on('click', function() {
         var title = $(this).data('title')
         $('#news .news-title').text(title)
+    })
+    //
+    var startX = 0
+    var endX = 0
+    var offsetDistance = 30
+    $('.carousel').on('touchstart', function(e) {
+      startX = e.originalEvent.touches[0].clientX
+    })
+    $('.carousel').on('touchmove', function (e) {
+      endX = e.originalEvent.touches[0].clientX
+    })
+    $('.carousel').on('touchend', function (e) {
+      if(Math.abs(endX - startX) > offsetDistance) {
+        $(this).carousel(endX > startX ? 'prev' : 'next')
+      }
     })
 })
